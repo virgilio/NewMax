270f7dd8abb0eb6ba5f42bb9579c20c362e6f4cb
virgilio <virgilio.santos@gmail.com>
2010-07-24 16:08:59 -0300

    mudanca no tipo de estado do cliente.

--- app/config/migrations/map.php
+++ app/config/migrations/map.php
@@ -6,5 +6,7 @@ $map = array(
 		'001_user_ativo' => 'M4c4b12db73f842c293dd2f1dd87f7e0c'),
 	3 => array(
 		'003_user_active_on' => 'M4c4b23ef91f840b6adbc31a9d87f7e0c'),
+	4 => array(
+		'004_user_active_manystates_on' => 'M4c4b3a329cbc42d2b0d035cbd87f7e0c'),
 );
 ?>

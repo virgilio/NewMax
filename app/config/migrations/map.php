7d1c265aea9542fc5ba0e9b1a36afa1e3457626b
virgilio <virgilio.santos@gmail.com>
2010-07-24 17:00:41 -0300

    please, lets do that magic now!

--- app/config/migrations/map.php
+++ app/config/migrations/map.php
@@ -1,14 +1,6 @@
 <?php
 $map = array(
 	1 => array(
-		'first' => 'M4c4b040bfbac411d84eb2be6d87f7e0c'),
-	2 => array(
-		'001_user_ativo' => 'M4c4b12db73f842c293dd2f1dd87f7e0c'),
-	3 => array(
-		'003_user_active_on' => 'M4c4b23ef91f840b6adbc31a9d87f7e0c'),
-	4 => array(
-		'004_user_active_manystates_on' => 'M4c4b3a329cbc42d2b0d035cbd87f7e0c'),
-	5 => array(
 		'005_user_fix' => 'M4c4b40df403447be9ed53791d87f7e0c'),
 );
 ?>

e93aec0853cf0fe4c2e0cdff1d3ddb41d1d06299
virgilio <virgilio.santos@gmail.com>
2010-07-24 16:39:05 -0300

    ¬¬'

---
+++ app/config/migrations/005_user_fix.php
@@ -0,0 +1,52 @@
+<?php
+class M4c4b40df403447be9ed53791d87f7e0c extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ * @access public
+ */
+	public $description = '';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ * @access public
+ */
+	public $migration = array(
+		'up' => array(
+			      'create_field' => array(
+						      'users' => array(
+									 'active' => array('type' => 'integer', 'null' => false, 'default' => NULL)
+									 ),
+						      ),
+		),
+		'down' => array(
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function after($direction) {
+		return true;
+	}
+}
+?>
